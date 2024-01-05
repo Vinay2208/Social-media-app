9fa693dc66c0230a1f8e2ded91f15207414785e4
Vinay2208 <vinayparam1998@gmail.com>
2024-01-05 22:51:28 +0530

    adding api config

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.members, name='members'),
-    path('members/', views.members, name='members')
+    path('members/', views.members, name='members'),
 ]
