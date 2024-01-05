1156d5d39ad464d69d4fc80ad50ceb0838a45ca1
Vinay2208 <vinayparam1998@gmail.com>
2024-01-05 23:20:29 +0530

    add a base template for the web pages

--- api/views.py
+++ api/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def members(request):
-    return render(request, "api/template.html")
+    return render(request, "api/home.html")
 
