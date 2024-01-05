45d54a2b63819c7b463dfd49977d64860907c5b0
Vinay2208 <vinayparam1998@gmail.com>
2024-01-06 00:00:01 +0530

    test home template with dummy data

--- api/views.py
+++ api/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {"author": "Author1",
+     "title": "Post1",
+     "content": "This is my first post!"},
+    {"author": "Author2",
+     "title": "Post2",
+     "content": "This is my second post!"},
+    {"author": "Author3",
+     "title": "Post3",
+     "content": "This is my third post!"}
+]
+
+
 def members(request):
-    return render(request, "api/home.html")
+    context = {
+        "posts": posts,
+        'title': "Latest Posts"
+    }
+    return render(request, "api/home.html", context)
 
